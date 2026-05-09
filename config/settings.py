1f4abb87f500e6da50100068c56e141b8bb17975
Ali Yar <yaralikhan116@gmail.com>
2026-05-09 22:55:39 +0100

    Fix database config for Postgres and SQLite

--- config/settings.py
+++ config/settings.py
@@ -59,16 +59,25 @@ TEMPLATES = [
 #     )
 # }
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=bool(os.environ.get("RENDER"))
-    )
-}
-
+if os.environ.get("DATABASE_URL"):
+    # ✅ Production (Render → PostgreSQL)
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # ✅ Local development (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
